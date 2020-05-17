167b48ba8dab25377d2a63e01741020dccf2d3ed
eve <evezyj0102@163.com>
2020-05-17 23:12:03 +0800

    Initial commit

---
+++ spring-cloud-biz-a/src/main/java/com/cloud/biz/a/ApplicationBizA.java
@@ -0,0 +1,12 @@
+package com.cloud.biz.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBizA {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBizA.class, args);
+    }
+}
+
---
+++ spring-cloud-biz-b/src/main/java/a/ApplicationBizB.java
@@ -0,0 +1,12 @@
+package a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBizB {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationBizB.class, args);
+    }
+}
+
